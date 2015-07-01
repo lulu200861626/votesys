359e71979908ab5420d9f5356583fe8316a99157
lulu05 <lulu05@meituan.com>
2015-07-01 21:16:24 +0800

    update

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,4 +2,14 @@
  * Created by lulu on 15/6/29.
  */
 public class Hello {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer id;
+
 }
